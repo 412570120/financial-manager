4dc7266097b76e98d281c5a57eeedceab3079377
芳 <a0907245996@gmail.com>
2024-11-29 17:17:42 +0800

    Update script.js

--- script.js
+++ script.js
@@ -98,7 +98,7 @@ document.addEventListener("DOMContentLoaded", () => {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
                 type: recordType,
-                menu: recordMenu,
+                item: recordMenu,
                 amount: recordAmount,
                 date: recordDate,
             }),
